8cfb8648b6fd2bd4d44ef31a001cf5d91473d64f
Sam Hagan <samhagan@Sams-MacBook-Pro.local>
2016-02-24 11:08:23 -0800

    completed revisions to webpack config creator

--- createWebpackConfig.js
+++ createWebpackConfig.js
@@ -1,32 +1,73 @@
 const fs = require('fs');
 
 // object constructor for loaders
-// these will also be exposed from the module
+// loaders need at least two props -- (i) loader and (ii) file extension to test
 function Loader() {
-  this.loader = [];
+  this.loaders = [];
   this.test = '';
-  this.query = '';
-  this.include = '';
 }
 Loader.prototype.addLoader = function(value) {
   //deal with both array and chained loaders
-  this.loader = value;
+  if (typeof value === 'string') {
+    value = value.split('!');
+  }
+
+  if (Array.isArray(value)) {
+    value.forEach(loader => this.loaders.push(loader.toString().trim()));
+    return;
+  }
 }
-Loader.prototype.addTest = function(value) {
-  this.test = value.toString();
+Loader.prototype.addTest = function(value, merge) {
+  if (merge && value) {
+    this.test = value.toString();
+    return;
+  }
+
+  if (value) {
+    // make sure that the input exists and is clean
+    value = value.toString().trim();
+
+    // add a period to the extension if not there
+    if (value[0] !== '.') value = '.' + value;
+
+    // add regex escapes
+    value = '/\\' + value + '/';
+    this.test = value;
+  }
 }
 Loader.prototype.addQuery = function(value) {
   this.query = value;
 }
-Loader.prototype.addInclude = function(value) {
-  this.include = value ? value : '/node_modules/';
+Loader.prototype.addInclude = function(value, merge) {
+  if (merge && value) {
+    this.include = value.toString().trim();
+    return;
+  }
+
+  // include should be regex otherwise not in the config
+  if (value) {
+    value = '/' + value.toString().trim() + '/';
+    this.include = value;
+  }
+}
+Loader.prototype.addExclude = function(value, merge) {
+  if (merge && value) {
+    this.exclude = value.toString().trim();
+    return;
+  }
+
+  // exclude should be regex otherwise not in the config
+  if (value) {
+    value = '/' + value.toString().trim() + '/';
+    this.exclude = value;
+  }
 }
 
 // object constructor for the output portion of the config file
+// there the minimum parameters are filename and path
 function Output() {
   this.filename = '';
   this.path = '';
-  this.publicPath = '';
 }
 Output.prototype.addFilename = function(value) {
   this.filename = value;
@@ -35,7 +76,8 @@ Output.prototype.addPath = function(value) {
   this.path = value;
 }
 Output.prototype.addPublicPath = function(value) {
-  this.publicPath = value;
+  // public path should be text; test before adding
+  if (value) this.publicPath = value.toString();
 }
 
 // primary config file object constructor
@@ -58,33 +100,29 @@ Config.prototype.addPlugin = function(value) {
   this.plugins.push(value);
 }
 
-Config.prototype.getPath = function() {
-
-}
-
-// to be added
-Config.prototype.removeLoader = function() {
-
-}
-
 // method to write the file to webpack.config.js file
-Config.prototype.done = function() {
-  fs.writeFile('test.js', objToString(this));
+Config.prototype.done = function(pathToFile) {
+  fs.writeFile(pathToFile + 'webpack.config2.js', objToString(this));
 }
 
 // could wrap in closure to avoid the history variable in the
 // main context
 const history = [];
+const regexCheck = {
+  'include': true,
+  'exclude': true,
+  'test': true
+};
 
 function objToString(obj, ndeep) {
   if(obj == null){ return String(obj); }
   switch(typeof obj){
     case "string":
     // deal with regex inputs
-    if (obj[0] === '/' && obj[obj.length - 1] === '/') {
+    if (obj[0] === '/' && obj[obj.length - 1] === '/' && regexCheck[history[history.length - 1]]) {
       // allows for modification to only do change for
       // properties that are predetermined to have regex
-      console.log(history.pop(), obj)
+      history.pop();
       return obj;
     }
     return "'"+obj+"'";
@@ -105,23 +143,26 @@ function objToString(obj, ndeep) {
 function createWebpackConfig(pathToFile) {
   // check for an existing webpack filename
   var existingConfig;
+  var existingRequires = [];
   try {
     existingConfig = require(pathToFile + '/webpack.config.js');
+
+    // also read file for any require statements
+    // fs.readFile
   }
   catch (e) {
     // no existing config file
   }
 
+  const newConfig = new Config();
   if (!existingConfig) {
     // when there is no existing config file return a new config object
-    return new Config();
+    return newConfig;
   }
 
-  const newConfig = new Config();
   // merge exisiting config into our structure
   for (var prop in existingConfig) {
-
-    // handle output object to grab methods
+    // handle objects where we want to use our methods
     if (prop === 'output') {
       newConfig.output.addPath(existingConfig[prop].path);
       newConfig.output.addPublicPath(existingConfig[prop].publicPath);
@@ -130,10 +171,11 @@ function createWebpackConfig(pathToFile) {
       existingConfig.module.loaders.forEach(loader => {
         const newLoader = newConfig.createLoader();
         newLoader.addLoader(loader.loader);
-        //newLoader.addLoader(loader.loaders);
-        newLoader.addTest(loader.test);
+        newLoader.addLoader(loader.loaders);
+        newLoader.addTest(loader.test, true);
         newLoader.addQuery(loader.query);
-        newLoader.addInclude(loader.include);
+        newLoader.addInclude(loader.include, true);
+        newLoader.addExclude(loader.exclude, true);
       });
     } else {
       newConfig[prop] = existingConfig[prop];
@@ -141,10 +183,9 @@ function createWebpackConfig(pathToFile) {
   }
 
   return newConfig;
-
 }
 
 const test = createWebpackConfig(__dirname);
-test.done();
+test.done('./');
 
 module.exports = createWebpackConfig;
