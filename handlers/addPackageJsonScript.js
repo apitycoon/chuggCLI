a5bf2d91756f5f1b9b3b2ee3611f0717a34e20a0
Ray Putt <rpputt@hotmail.com>
2016-02-24 13:24:47 -0800

    adds webpack watch to package.json

--- handlers/addPackageJsonScript.js
+++ handlers/addPackageJsonScript.js
@@ -2,14 +2,13 @@ const fs = require('fs');
 const path = require('path');
 
 function readPackage() {
-  var data = fs.readFile(path.join(__dirname, '../package.json'),(err, data) => {
+  var data = fs.readFile(path.join(__dirname, '../package.json'), (err, data) => {
     var package = JSON.parse(data)
     package.scripts['webpack-watch'] = 'webpack -w';
     var stringPackage = JSON.stringify(package, null, 4);
     fs.writeFile(path.join(__dirname, '../package.json'), stringPackage, (err) => {
-      if(err) throw err;
+      if (err) throw err;
     })
-
   })
 }
 
